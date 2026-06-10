09fdd321f07d483825baa8e0b45f8d1cc60275f4
Dimas <dimas.ciputra@gmail.com>
2026-06-10 21:21:11 +0100

    Only enable save button after a successful connection test

--- credentials_dialog.py
+++ credentials_dialog.py
@@ -3,7 +3,7 @@ import shutil
 
 from qgis.PyQt import uic
 from qgis.PyQt.QtCore import Qt
-from qgis.PyQt.QtWidgets import QDialog, QMessageBox
+from qgis.PyQt.QtWidgets import QDialog, QDialogButtonBox, QMessageBox
 
 FORM_CLASS, _ = uic.loadUiType(
     os.path.join(os.path.dirname(__file__), "gui", "credentials_dialog.ui")
@@ -20,10 +20,16 @@ class CredentialsDialog(QDialog, FORM_CLASS):
             Qt.TextSelectableByMouse | Qt.TextSelectableByKeyboard
         )
 
+        self._test_passed = False
+        self._save_btn = self.button_box.button(QDialogButtonBox.Save)
+        self._save_btn.setEnabled(False)
+
         self.test_btn.clicked.connect(self._test_connection)
         self.logout_btn.clicked.connect(self._logout)
         self.button_box.accepted.connect(self._save)
         self.button_box.rejected.connect(self.reject)
+        self.username_edit.textChanged.connect(self._on_credentials_changed)
+        self.password_edit.textChanged.connect(self._on_credentials_changed)
 
         self._load()
 
@@ -33,7 +39,13 @@ class CredentialsDialog(QDialog, FORM_CLASS):
         self.username_edit.setText(username)
         self.password_edit.setText(password)
 
+    def _on_credentials_changed(self):
+        self._test_passed = False
+        self._save_btn.setEnabled(False)
+
     def _save(self):
+        if not self._test_passed:
+            return
         from .gbif_api import save_credentials
         save_credentials(
             self.username_edit.text().strip(),
@@ -82,6 +94,8 @@ class CredentialsDialog(QDialog, FORM_CLASS):
             return
 
         ok, message = test_credentials(username, password)
+        self._test_passed = ok
+        self._save_btn.setEnabled(ok)
         self.result_label.setText(message)
         self.result_label.setStyleSheet("color: green;" if ok else "color: red;")
         self.test_btn.setEnabled(True)
